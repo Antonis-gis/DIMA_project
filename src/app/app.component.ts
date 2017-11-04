8877e5675e5a81fa6266eade9c6ad531364a17fb
Elena <Iwetta@yandex.ru>
2017-11-04 20:11:06 +0100

    Removed ignored files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import firebase from 'firebase';
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -9,6 +10,28 @@ import { HomePage } from '../pages/home/home';
 })
 export class MyApp {
   rootPage:any = HomePage;
+
+constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    var config = {
+    apiKey: "AIzaSyCkVY5E3kMJmLT3A9dL-MtpPFMKktEysyk",
+    authDomain: "dima-b17a1.firebaseapp.com",
+    databaseURL: "https://dima-b17a1.firebaseio.com",
+    projectId: "dima-b17a1",
+    storageBucket: "dima-b17a1.appspot.com",
+    messagingSenderId: "656945745490"
+  };
+  firebase.initializeApp(config);
+
+
+    const unsubscribe = firebase.auth().onAuthStateChanged( user => {
+      if (user){
+        this.rootPage = HomePage;
+        unsubscribe();
+      } else {
+        this.rootPage = 'LoginPage';
+        unsubscribe();
+      }
+});
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
